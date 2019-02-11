cc079e3ab76903cbbc4f098ef20b4360f748a257
shokhapro <41318952+shokhapro@users.noreply.github.com>
2019-02-11 13:47:57 +0500

    Update vars names
    Levon, Thanks for remark!

--- URandom.cs
+++ URandom.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class URandom
@@ -9,12 +9,12 @@ public class URandom
 		_array = new int[max - min + 1];
 		for (var i = 0; i < _array.Length; i++) _array[i] = min + i;
 		ResetList();
-		_l = _n = 0;
+		_lastId = _nextId = 0;
 	}
 
 	private int[] _array;
 	private List<int> _list;
-	private int _l, _n;
+	private int _lastId, _nextId;
 
 	private void ResetList()
 	{
@@ -25,22 +25,22 @@ public class URandom
 	private int GetRandomFromList()
 	{
 		if (_list.Count.Equals(0)) ResetList();
-		var r = _l;
-		if (_list.Count.Equals(1)) r = 0;
-		else while (r.Equals(_l))
-			r = Random.Range(0, _list.Count);
-		var value = _list[r];
-		_list.RemoveAt(r);
+		var randId = _lastId;
+		if (_list.Count.Equals(1)) randId = 0;
+		else while (randId.Equals(_lastId))
+			randId = Random.Range(0, _list.Count);
+		var value = _list[randId];
+		_list.RemoveAt(randId);
 		return value;
 	}
 
 	public int Next()
 	{
-		_l = _n;
-		_n = GetRandomFromList();
+		_lastId = _nextId;
+		_nextId = GetRandomFromList();
 		return next;
 	}
 
-	public int last { get { return _array[_l]; }}
-	public int next { get { return _array[_n]; }}
+	public int last { get { return _array[_lastId]; }}
+	public int next { get { return _array[_nextId]; }}
 }
