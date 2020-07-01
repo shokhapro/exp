2957f1f490d4d1ff3761d6dca628575b73cdd2cb
shokhapro <41318952+shokhapro@users.noreply.github.com>
2020-07-02 04:00:43 +0500

    Add files via upload

---
+++ ui/RectSizeFitter.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+[RequireComponent(typeof(RectTransform))]
+public class RectSizeFitter : MonoBehaviour
+{
+    [SerializeField] private RectTransform fitObject = null;
+    [SerializeField] private bool horizontalFit = false;
+    [SerializeField] private float horizontalAdd = 0f;
+    [SerializeField] private bool verticalFit = false;
+    [SerializeField] private float verticalAdd = 0f;
+
+    private RectTransform _rt;
+    private Vector2 _size;
+
+    private void Awake()
+    {
+        _rt = GetComponent<RectTransform>();
+        _size = _rt.sizeDelta;
+    }
+
+    private void OnGUI()
+    {
+        if (fitObject != null && fitObject.sizeDelta != _size)
+        {
+            _size = fitObject.sizeDelta;
+
+            Fit();
+        }
+    }
+
+    private void Fit()
+    {
+        if (_rt == null) return;
+
+        if (horizontalFit) _rt.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, _size.x + horizontalAdd);
+        if (verticalFit) _rt.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, _size.y + verticalAdd);
+    }
+
+#if UNITY_EDITOR
+    private void OnValidate()
+    {
+        Fit();
+    }
+#endif
+}
